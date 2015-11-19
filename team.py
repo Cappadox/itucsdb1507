9e75b9ac097f5d12ce2dc68ed2860f30e7f9bff9
erensahin <sahineren.09@gmail.com>
2015-11-19 21:30:54 +0200

    team class created

---
+++ team.py
@@ -0,0 +1,7 @@
+class Team:
+    def __init__(self, id, name, year, standing, avgfan):
+        self.id = id
+        self.name = name
+        self.year = year
+        self.standing = standing
+        self.avgfan = avgfan
