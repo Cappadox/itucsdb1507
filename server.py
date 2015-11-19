c99b0091b5380cb97c559ba935c0e35fe52e83fc
erensahin <sahineren.09@gmail.com>
2015-11-20 00:31:06 +0200

    added elephantsql code

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 import datetime
 import os
-#import psycopg2 as dbb
+import psycopg2 as dbapi
 
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
+
+
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 
 @app.route('/')
@@ -24,5 +35,17 @@ def layout():
     return render_template('layout.html')
 
 if __name__ == '__main__':
-    PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
-    app.run(host='0.0.0.0', port=int(PORT))
+    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
+    if VCAP_APP_PORT is not None:
+        port, debug = int(VCAP_APP_PORT), False
+    else:
+        port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=54321 dbname='itucsdb'"""
+
+    app.run(host='0.0.0.0', port=port, debug=debug)
