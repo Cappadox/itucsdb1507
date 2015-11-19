12202480ae5de59c338e51e629a3bb5f9ca66e06
erensahin <sahineren.09@gmail.com>
2015-11-19 21:35:31 +0200

    player class created

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, id, name, teamid, age, kitno):
+        self.id = id
+        self.name = name
+        self.teamid = teamid
+        self.age = age
+        self.kitno = kitno
