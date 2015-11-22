73a80b2873bdf8c10d09da6e7813f713352da495
kubilay <karpatkubilay@gmail.com>
2015-11-22 20:53:56 +0200

    server.py and layout.html organized in alphabetic order

--- server.py
+++ server.py
@@ -10,17 +10,17 @@ from flask import render_template
 from flask import request
 from flask.helpers import url_for
 
-from officials import Officials, Official
+
 from countries import Countries, Country
 from matches import Matches, Match
 from teams import Team, Teams
+from officials import Officials, Official
 from players import Player, Players
 from statistics import Statistic, Statistics
 
-
 app = Flask(__name__)
 
-
+'''ElephantSQL configurations'''
 def get_elephantsql_dsn(vcap_services):
     """Returns the data source name for ElephantSQL."""
     parsed = json.loads(vcap_services)
@@ -31,21 +31,56 @@ def get_elephantsql_dsn(vcap_services):
              dbname='{}'""".format(user, password, host, port, dbname)
     return dsn
 
-
+'''Home Page'''
 @app.route('/')
 def home_page():
     return render_template('home.html')
 
-@app.route('/teams', methods=['GET', 'POST'])
-def teams():
+
+'''Coaches Pages'''
+@app.route('/coaches')
+def coaches():
+    now = datetime.datetime.now()
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """ SELECT * FROM COACHES"""
+        cursor.execute(query)
+        result = cursor.fetchall()
+    return render_template('coaches.html', current_time=now.ctime(), result = result)
+
+@app.route('/addcoach', methods = ['GET', 'POST'])
+def add_coach():
+        name = request.form.get("coachName")
+        birthday = request.form.get("coachBirthday")
+
+        '''TODO @ilay : add method for adding operations'''
+        with dbapi2.connect(app.config['dsn']) as connection:
+            cursor = connection.cursor()
+
+            query = """ INSERT INTO COACHES (NAME, BIRTHDAY) VALUES (%s, %s)"""
+            cursor.execute(query, (name, birthday))
+            connection.commit()
+
+        return redirect(url_for('coaches'))
+
+
+'''Countries Pages'''
+@app.route('/countries', methods=['GET', 'POST'])
+def countries():
     if request.method == 'GET':
-        return render_template('teams.html', result = app.teams.select_teams())
+        return render_template('countries.html', countries = app.countries.get_countries())
     else:
         name = request.form['name']
-        league_id = request.form['league_id']
-        app.teams.add_team(name,league_id)
-    return redirect(url_for('teams'))
+        abbreviation = request.form['abbreviation']
+        app.countries.add_country(name, abbreviation)
+        return redirect(url_for('countries'))
 
+@app.route('/countries/add')
+def country_edit_page():
+     return render_template('country_edit.html')
+
+
+'''Player Pages'''
 @app.route('/players', methods=['GET', 'POST'])
 def players():
     if request.method == 'GET':
@@ -57,6 +92,8 @@ def players():
         app.players.add_player(name, birthday, position)
     return redirect(url_for('players'))
 
+
+'''Statistics Pages'''
 @app.route('/statistics', methods = ['GET', 'POST'])
 def statistics():
     if request.method == 'GET':
@@ -70,35 +107,25 @@ def statistics():
     return redirect(url_for('statistics'))
 
 
-@app.route('/coaches')
-def coaches():
-    now = datetime.datetime.now()
-    with dbapi2.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """ SELECT * FROM COACHES"""
-        cursor.execute(query)
-        result = cursor.fetchall()
-    return render_template('coaches.html', current_time=now.ctime(), result = result)
-
-
-@app.route('/countries', methods=['GET', 'POST'])
-def countries_page():
+'''Team pages'''
+@app.route('/teams', methods=['GET', 'POST'])
+def teams():
     if request.method == 'GET':
-        return render_template('countries.html', countries = app.countries.get_countries())
+        return render_template('teams.html', result = app.teams.select_teams())
     else:
         name = request.form['name']
-        abbreviation = request.form['abbreviation']
-        app.countries.add_country(name, abbreviation)
-        return redirect(url_for('countries_page'))
+        league_id = request.form['league_id']
+        app.teams.add_team(name,league_id)
+    return redirect(url_for('teams'))
 
-@app.route('/countries/add')
-def country_edit_page():
-     return render_template('country_edit.html')
-
+'''Database initialization'''
 @app.route('/initdb')
 def create_tables():
+
+    '''TODO,BEGIN A method to initialize coaches table, then delete this part'''
     with dbapi2.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
+
 
         query = """CREATE TABLE IF NOT EXISTS COACHES
         (
@@ -109,38 +136,29 @@ def create_tables():
         cursor.execute(query)
 
         connection.commit()
+    '''TODO,END'''
 
-        app.teams.initialize_tables()
-        app.players.initialize_tables()
-        app.countries.initialize_tables()
-        app.officials.initialize_tables()
-        app.matches.initialize_tables()
-        app.statistics.initialize_tables()
+    '''Reference order in DB should be preserved'''
+    app.teams.initialize_tables()
+    app.players.initialize_tables()
+    app.countries.initialize_tables()
+    app.officials.initialize_tables()
+    app.matches.initialize_tables()
+    app.statistics.initialize_tables()
 
     return redirect(url_for('home_page'))
 
-@app.route('/addcoach', methods = ['GET', 'POST'])
-def add_coach():
-        name = request.form.get("coachName")
-        birthday = request.form.get("coachBirthday")
-
-
-        with dbapi2.connect(app.config['dsn']) as connection:
-            cursor = connection.cursor()
-
-            query = """ INSERT INTO COACHES (NAME, BIRTHDAY) VALUES (%s, %s)"""
-            cursor.execute(query, (name, birthday))
-            connection.commit()
-
-        return redirect(url_for('coaches'))
 
 if __name__ == '__main__':
+
+    '''Container objects'''
     app.teams = Teams(app)
     app.players = Players(app)
     app.countries = Countries(app)
     app.officials = Officials(app)
     app.matches = Matches(app)
     app.statistics = Statistics(app)
+
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
