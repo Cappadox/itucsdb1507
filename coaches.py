4fec3df1a573e22ca56efeed69bfb28213bfb218
İlay <koksalil@itu.edu.tr>
2015-11-23 01:16:25 +0200

    -delete operatiton for coaches table & some edit-

--- coaches.py
+++ coaches.py
@@ -1,5 +1,44 @@
+import psycopg2 as dbapi2
+
 class Coaches:
     def __init__(self, id, name,birthday):
         self.id = id
         self.name = name
         self.birthday = birthday
+
+class Coaches2:
+    def __init__(self, app):
+        self.app = app
+
+    def initialize_tables(self):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+                cursor = connection.cursor()
+                cursor.execute("""CREATE TABLE IF NOT EXISTS COACHES
+                (
+                COACH_ID SERIAL PRIMARY KEY,
+                NAME VARCHAR(50) NOT NULL,
+                BIRTHDAY INTEGER NOT NULL
+                ) """)
+                connection.commit()
+
+    def select_coaches(self):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+             cursor = connection.cursor()
+             query = """ SELECT * FROM COACHES"""
+             cursor.execute(query)
+             result = cursor.fetchall()
+             return result
+
+    def add_coach(self, name, birthday):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+                cursor = connection.cursor()
+                query = """ INSERT INTO COACHES (NAME, BIRTHDAY) VALUES (%s, %s) """
+                cursor.execute(query, (name, birthday))
+                connection.commit()
+
+    def delete_coach(self, id):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+                cursor = connection.cursor()
+                query = """ DELETE FROM COACHES WHERE ID =%s """
+                cursor.execute(query, [id])
+                connection.commit()
--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from flask import render_template
 from flask import request
 from flask.helpers import url_for
 
+from coaches import Coaches, Coaches2
 from countries import Countries, Country
 from leagues import Leagues, League
 from matches import Matches, Match
@@ -40,30 +41,22 @@ def home_page():
 
 
 '''Coaches Pages'''
-@app.route('/coaches')
+@app.route('/coaches', methods=['GET', 'POST'])
 def coaches():
-    now = datetime.datetime.now()
-    with dbapi2.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """ SELECT * FROM COACHES"""
-        cursor.execute(query)
-        result = cursor.fetchall()
-    return render_template('coaches.html', current_time=now.ctime(), result = result)
-
-@app.route('/addcoach', methods = ['GET', 'POST'])
-def add_coach():
-        name = request.form.get("coachName")
-        birthday = request.form.get("coachBirthday")
-
-        '''TODO @ilay : add method for adding operations'''
-        with dbapi2.connect(app.config['dsn']) as connection:
-            cursor = connection.cursor()
-
-            query = """ INSERT INTO COACHES (NAME, BIRTHDAY) VALUES (%s, %s)"""
-            cursor.execute(query, (name, birthday))
-            connection.commit()
-
-        return redirect(url_for('coaches'))
+    if request.method == 'GET':
+        return render_template('coaches.html', result = app.coaches.select_coaches())
+    else:
+        if 'Add' in request.form:
+            name = request.form['coachName']
+            birthday = request.form['coachBirthday']
+            app.coaches.add_coach(name, birthday)
+            return redirect(url_for('coaches'))
+        elif 'Delete' in request.form:
+            id = request.form['id']
+            app.coaches.delete_coach(id)
+            return redirect(url_for('coaches'))
+        else:
+            return render_template('coaches.html', result = app.coaches.select_coaches())
 
 
 '''Countries Pages'''
@@ -169,23 +162,8 @@ def teams():
 @app.route('/initdb')
 def create_tables():
 
-    '''TODO,BEGIN A method to initialize coaches table, then delete this part'''
-    with dbapi2.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-
-
-        query = """CREATE TABLE IF NOT EXISTS COACHES
-        (
-        COACH_ID SERIAL PRIMARY KEY,
-        NAME VARCHAR(50) NOT NULL,
-        BIRTHDAY INTEGER NOT NULL
-        ) """
-        cursor.execute(query)
-
-        connection.commit()
-    '''TODO,END'''
-
     '''Reference order in DB should be preserved'''
+    app.coaches.initialize_tables()
     app.countries.initialize_tables()
     app.leagues.initialize_tables()
     app.teams.initialize_tables()
@@ -200,6 +178,7 @@ def create_tables():
 if __name__ == '__main__':
 
     '''Container objects'''
+    app.coaches = Coaches2(app)
     app.teams = Teams(app)
     app.players = Players(app)
     app.countries = Countries(app)
