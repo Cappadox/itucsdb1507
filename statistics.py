4ac7cb1fb5c12c977bfbe723143194cdd8362c5d
sedayildirim <yildirimsed@itu.edu.tr>
2015-11-24 14:41:08 +0200

    Delete added for statistics page, main statistics page changed.

--- statistics.py
+++ statistics.py
@@ -1,11 +1,12 @@
 import psycopg2 as dbapi2
 
 class Statistic:
-    def __init__(self, season, playerName, receptions, receivingyards):
+    def __init__(self, statistic_id, season, playerName, touchdowns, rushingYards):
+        self.statistic_id = statistic_id
         self.season = season
         self.playerName = playerName
-        self.receptions = receptions
-        self.receivingyards = receivingyards
+        self.touchdowns = touchdowns
+        self.rushingYards = rushingYards
 
 class Statistics:
     def __init__(self, app):
@@ -17,10 +18,11 @@ class Statistics:
                 cursor.execute("""
                     CREATE TABLE IF NOT EXISTS STATISTICS
                     (
-                    SEASON INTEGER NOT NULL,
-                    PLAYERNAME VARCHAR(50) NOT NULL,
-                    RECEPTIONS INTEGER NOT NULL,
-                    RECEIVINGYARDS INTEGER NOT NULL
+                    STATISTIC_ID SERIAL NOT NULL PRIMARY KEY,
+                    SEASON_ID INTEGER NOT NULL REFERENCES SEASONS(SEASON_ID),
+                    TEAM_ID INTEGER NOT NULL REFERENCES TEAMS(TEAM_ID),
+                    touchdowns INTEGER NOT NULL,
+                    rushingYards INTEGER NOT NULL
                     )
                     """)
                 connection.commit()
@@ -33,9 +35,18 @@ class Statistics:
             result = cursor.fetchall()
             return result
 
-    def add_statistic(self, season, playerName, receptions, receivingyards):
+    def add_statistic(self, season_id, team_id, touchdowns, rushingYards):
         with dbapi2.connect(self.app.config['dsn']) as connection:
                 cursor = connection.cursor()
-                query = """ INSERT INTO STATISTICS (SEASON, PLAYERNAME, RECEPTIONS, RECEIVINGYARDS) VALUES (%s, %s, %s, %s) """
-                cursor.execute(query, (season, playerName, receptions, receivingyards))
+                query = """ INSERT INTO STATISTICS (SEASON_ID, TEAM_ID, touchdowns, rushingYards) VALUES (%s, %s, %s, %s) """
+                cursor.execute(query, (season_id, team_id, touchdowns, rushingYards))
+                connection.commit()
+
+    def delete_statistic(self, id):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+                cursor = connection.cursor()
+                cursor.execute("""
+                    DELETE FROM STATISTICS
+                    WHERE STATISTIC_ID = %s""",
+                    id)
                 connection.commit()
