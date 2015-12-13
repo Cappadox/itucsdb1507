af24a6e690577479a1d2e8fd3cbe4aead9700fc1
erensahin <sahineren.09@gmail.com>
2015-12-13 23:42:04 +0200

    added required constraint to add forms for squad, player, team tables' not nullable columns.

--- players.py
+++ players.py
@@ -18,8 +18,8 @@ class Players:
                     CREATE TABLE IF NOT EXISTS PLAYERS
                     ( PLAYER_ID serial NOT NULL PRIMARY KEY,
                       NAME varchar(100) NOT NULL,
-                      BIRTHDAY date NULL,
-                      POSITION varchar(100) NULL
+                      BIRTHDAY date NOT NULL,
+                      POSITION varchar(100) NOT NULL
                     )
                     """)
                 connection.commit()
