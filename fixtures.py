966c500ad657439f78ac34d33be7737c433f23b6
sedayildirim <yildirimsed@itu.edu.tr>
2015-12-13 17:55:59 +0200

    fixtures search fixed.

--- fixtures.py
+++ fixtures.py
@@ -70,9 +70,11 @@ class Fixtures:
     def search_fixture(self, id):
         with dbapi2.connect(self.app.config['dsn']) as connection:
             cursor = connection.cursor()
-            query="""SELECT * FROM FIXTURES
-                    WHERE CAST(TEAM_ID as VARCHAR(30)) LIKE '%s'
-                    ORDER BY TEAM_ID ASC""" % (('%'+id+'%'))
+            query="""SELECT FIXTURE_ID, SEASONS.YEAR, TEAMS.NAME, POINTS
+                    FROM FIXTURES
+                    INNER JOIN SEASONS ON SEASONS.SEASON_ID=FIXTURES.SEASON_ID
+                    INNER JOIN TEAMS ON TEAMS.TEAM_ID=FIXTURES.TEAM_ID
+                    WHERE TEAMS.NAME LIKE '%s'""" % ('%'+id+'%')
             cursor.execute(query)
             connection.commit()
 
