7cc612fd4ecbb3f6a9f666b2d989f577e14095fd
erensahin <sahineren.09@gmail.com>
2015-12-19 15:56:37 +0200

    documentation started. edited member5.rst, index and conf.

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'itucsdb'
-copyright = '2015, Team Name'
+project = 'itucsdb1507'
+copyright = '2015, itucsdb1507'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -177,7 +177,7 @@ html_use_index = False
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'itucsdbdoc'
+htmlhelp_basename = 'itucsdb1507doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -197,7 +197,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'itucsdb.tex', 'itucsdb Documentation',
+  ('index', 'itucsdb1507.tex', 'itucsdb1507 Documentation',
    'Team Name', 'manual'),
 ]
 
@@ -227,7 +227,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'itucsdb', 'itucsdb Documentation',
+    ('index', 'itucsdb1507', 'itucsdb1507 Documentation',
      ['Team Name'], 1)
 ]
 
@@ -241,8 +241,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'itucsdb', 'itucsdb Documentation',
-   'Team Name', 'itucsdb', 'One line description of project.',
+  ('index', 'itucsdb1507', 'itucsdb1507 Documentation',
+   'Team Name', 'itucsdb1507', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- docs/developer/member5.rst
+++ docs/developer/member5.rst
@@ -1,2 +1,19 @@
-Parts Implemented by Member Name
-================================
+Parts Implemented by Sefa Eren Şahin
+====================================
+
+**Players, Teams and Squad tables are implemented.**
+
+**Players Table**
+
+Table is created by following sql code::
+
+   code-block:: python
+   CREATE TABLE IF NOT EXISTS PLAYERS
+                       ( PLAYER_ID serial NOT NULL PRIMARY KEY,
+                         NAME varchar(100) NOT NULL,
+                         BIRTHDAY date NOT NULL,
+                         POSITION varchar(100) NOT NULL
+                       )
+
+asd
+
